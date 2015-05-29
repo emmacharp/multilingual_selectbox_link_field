cff6b1fb807b12a8be8b22debc778e5be7e49bed
Nicolas Brassard <nico@nitriques.com>
2015-05-29 13:16:15 -0400

    fix install problems

--- extension.driver.php
+++ extension.driver.php
@@ -6,6 +6,11 @@
 	
 	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
 	
+	if (!class_exists('extension_selectbox_link_field')) {
+		define('__SBLF_NOT_INSTALLED', 1);
+		class extension_selectbox_link_field extends Extension {}
+	}
+	
 	/**
 	 *
 	 * @author Deux Huit Huit
@@ -26,28 +31,24 @@
 		 * Creates the table needed for the settings of the field
 		 */
 		public function install() {
-			$__select_box_link_loaded = false;
-			try {
-				require_once(EXTENSIONS . '/selectbox_link_field/extension.driver.php');
-				require_once(EXTENSIONS . '/selectbox_link_field/fields/field.selectbox_link.php');
-				$__select_box_link_loaded = true;
-			} catch (Exception $ex) {
-				$__select_box_link_loaded = false;
-			}
-			if ($__select_box_link_loaded != true) {
-				Administration::instance()->Page->pageAlert('Could not load selectbox_link_field extension.', Alert::ERROR);
-				return false;
+			if (__SBLF_NOT_INSTALLED) {
+				$sbl_status = ExtensionManager::fetchStatus(array('handle' => 'selectbox_link_field'));
+				$sbl_status = current($sbl_status);
+				if ($sbl_status != EXTENSION_ENABLED) {
+					Administration::instance()->Page->pageAlert('Could not load `selectbox_link_field` extension.', Alert::ERROR);
+					return false;
+				}
 			}
 			// depends on "Languages"
 			$languages_status = ExtensionManager::fetchStatus(array('handle' => 'languages'));
 			$languages_status = current($languages_status);
 			if ($languages_status != EXTENSION_ENABLED) {
-				Administration::instance()->Page->pageAlert('Could not load languages extension.', Alert::ERROR);
+				Administration::instance()->Page->pageAlert('Could not load `languages` extension.', Alert::ERROR);
 				return false;
 			}
 			
 			// create table "alias"
-			Administration::instance()->Database()->query("CREATE VIEW `tbl_fields_multilingual_selectbox_link` AS
+			Administration::instance()->Database()->query("CREATE OR REPLACE VIEW `tbl_fields_multilingual_selectbox_link` AS
 				SELECT * FROM `tbl_fields_selectbox_link`;");
 			
 			return true;
@@ -64,7 +65,7 @@
 		}
 
 		public function uninstall() {
-			Administration::instance()->Database()->query("DROP VIEW `tbl_fields_multilingual_selectbox_link`");
+			Administration::instance()->Database()->query("DROP VIEW IF EXISTS `tbl_fields_multilingual_selectbox_link`");
 			return true;
 		}
 
